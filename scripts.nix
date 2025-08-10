2e6e9c1aa6826195997842ba426605ed5761c9fd
Davide Cuna <davide.cuna0503@gmail.com>
2025-08-11 00:47:58 +0200

    update

--- scripts.nix
+++ scripts.nix
@@ -8,18 +8,24 @@
   home.file."${config.home.homeDirectory}/.local/bin/pomodoro.sh" = {
     text = ''
       #!/usr/bin/env bash
-      TIMER_FILE = "/tmp/waybar-pomodoro.timer"
-      DURATION = 1500
 
-      if [[ "$1" == start ]]; then
+      TIMER_FILE="/tmp/waybar-pomodoro.timer"
+      DURATION=1500
+
+      if [[ "$1" == "start" ]]; then
         date +%s > "$TIMER_FILE"
         exit 0
       fi 
 
+      if [[ "$1" == "reset" ]]; then
+        rm -f "$TIMER_FILE"
+        exit 0
+      fi 
+
       if [[ -f "$TIMER_FILE" ]]; then
-        START = $(cat "$TIMER_FILE")
-        NOW = $(date +%s)
-        REMAIN = $((DURATION - (NOW - START) ))
+        START=$(cat "$TIMER_FILE")
+        NOW=$(date +%s)
+        REMAIN=$((DURATION - (NOW - START) ))
         if ((REMAIN > 0)); then
           printf '{"text":"%02d:%02d"}\n' $((REMAIN/60)) $((REMAIN%60))
         else 
